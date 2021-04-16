8af70e05bb8e4e914ff966ac0e36b291e6bef65b
Duy Pham Le <phamleduy04@gmail.com>
2021-04-15 19:59:27 -0500

    add

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const sslRedirect = require('heroku-ssl-redirect').default;
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -5,18 +6,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
 require('dotenv').config();
-const { NODE_ENV = 'development' } = process.env;
 
-// force SSL
-const forceSSL = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  return next();
-};
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(sslRedirect());
 app.use(express.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -30,7 +26,6 @@ console.log(path.join(__dirname, '..', 'build'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
-if (NODE_ENV === 'production') app.use(forceSSL);
 app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
