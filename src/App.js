1deda3143cad3c6ebbe46c698e11fdaf1188f901
Duy Pham Le <phamleduy04@gmail.com>
2021-04-16 10:47:38 -0500

    add reactGA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect } from 'react';
-import { useLocation, Switch } from 'react-router-dom';
+import { useLocation, Switch, Router } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
 import ReactGA from 'react-ga';
-
+import { createBrowserHistory } from 'history';
 // Layouts
 import LayoutDefault from './layouts/LayoutDefault';
 
@@ -13,32 +13,36 @@ import Home from './views/Home';
 //Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
 
-const trackPage = page => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
+const history = createBrowserHistory();
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 
 const App = () => {
 
   const childRef = useRef();
   let location = useLocation();
-
+  
   useEffect(() => {
     document.body.classList.add('is-loaded')
     childRef.current.init();
-    trackPage(location.pathname);
+    //trackPage(location.pathname);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
   return (
-    <ScrollReveal
-      ref={childRef}
-      children={() => (
-        <Switch>
-          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
-        </Switch>
-      )} />
+    <Router history={history}>
+      <ScrollReveal
+        ref={childRef}
+        children={() => (
+          <Switch>
+            <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+          </Switch>
+        )} />
+    </Router>
   );
 }
 
